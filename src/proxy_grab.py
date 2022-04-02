de37e3bf45262c95943a1d6ee9ae2d7a90adbdca
rodukov <electroneuphoria@gmail.com>
2022-04-02 13:30:51 +0300

    comment

--- src/proxy_grab.py
+++ src/proxy_grab.py
@@ -6,36 +6,36 @@ from src.config import _blacklisted, _connection_protocol, _anonymity_type, _pro
 class proxyGrab:
     class hidemyname:
         def request(page: int=1):
-            
-            d = 64
-            url_prefix = ""
-            if page > 2:
-                a = d*(page-1) # formula of the progressiong which calculating...
-                url_prefix = f"?start={str(a)}#list"
-            elif page == 2:
-                url_prefix = f"?start={str(d)}#list"
+            """This function makes a request and returns a response from the resource"""
+            url_prefix = "" # Used for content filtering and other results. In essence, it is an expanded capability to produce results
+            d = 64 # Progression difference
+            if page > 2: # NOTE: Change this > to this >=
+                a = d*(page-1) # Calculate the required term of the progression using the formula
+                url_prefix = f"?start={str(a)}#list" # Adding the result to the link prefix
+            elif page == 2: # NOTE: I know that this is the same as above, it is important to remove in the future
+                url_prefix = f"?start={str(d)}#list" # Add at once the difference of the progression
 
             url = "https://hidemy.name/en/proxy-list/" + url_prefix
-            headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+            headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'} # Be sure to add a browser header to bypass protection
             request = get(url, headers=headers)
             return request
         def sort(request) -> dict:
-
+            """This function converts the query result into a conveniently readable dictionary"""
             soup = BeautifulSoup(request.text, 'html.parser')
             all = soup.find_all('td')
-            result = []
-            result_item = {}
+            result = [] # This list will be returned by this function
+            result_item = {} # One specific item will be stored here
             for i in all:
                 i = str(i)
                 if "<td>" in i:
-                    i = i.replace("<td>", "").replace("</td>", "")
-                if i not in _blacklisted and "div" not in i:
-                    if "." in i:
+                    i = i.replace("<td>", "").replace("</td>", "") # Remove the opening and closing HTML tags <td> and </td>.
+                if i not in _blacklisted and "div" not in i: # Remove blocked words (watch src/config.py)
+                    if "." in i: # This is the IP address
                         result_item["address"] = i
-                    if i.isdigit():
+                    if i.isdigit(): # It's a port.
                         result_item["port"] = i
                     elif "protocol" not in result_item:
-                        if i in _connection_protocol:
+                        if i in _connection_protocol: # It's protocol.
                             result_item["protocol"] = i
                     else:
                         if result_item != {}:
